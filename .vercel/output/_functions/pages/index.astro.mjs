0f6bcb37ff45788d991b94302685bc75b2fcad8c
brian-mati <brianmuthomimati@gmail.com>
2025-02-25 10:27:57 +0300

    blog and about us page

---
+++ .vercel/output/_functions/pages/index.astro.mjs
@@ -0,0 +1,25 @@
+import { c as createComponent, a as createAstro, b as addAttribute, r as renderHead, d as renderTemplate } from '../chunks/astro/server_eUmav9IZ.mjs';
+import 'kleur/colors';
+import 'clsx';
+export { renderers } from '../renderers.mjs';
+
+const $$Astro = createAstro();
+const $$Index = createComponent(($$result, $$props, $$slots) => {
+  const Astro2 = $$result.createAstro($$Astro, $$props, $$slots);
+  Astro2.self = $$Index;
+  return renderTemplate`<html lang="en"> <head><meta charset="utf-8"><link rel="icon" type="image/svg+xml" href="/favicon.svg"><meta name="viewport" content="width=device-width"><meta name="generator"${addAttribute(Astro2.generator, "content")}><title>Astro Site</title>${renderHead()}</head> <body> <h1>Astro</h1> </body></html>`;
+}, "/home/debian-desktop/astro-person/astro-blog/src/pages/index.astro", void 0);
+
+const $$file = "/home/debian-desktop/astro-person/astro-blog/src/pages/index.astro";
+const $$url = "";
+
+const _page = /*#__PURE__*/Object.freeze(/*#__PURE__*/Object.defineProperty({
+	__proto__: null,
+	default: $$Index,
+	file: $$file,
+	url: $$url
+}, Symbol.toStringTag, { value: 'Module' }));
+
+const page = () => _page;
+
+export { page };
